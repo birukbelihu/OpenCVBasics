e47be5c5e948f4bb51926d6e739bb0232ba91898
Biruk Belihu <birukbelihu1995@gmail.com>
2025-07-06 10:28:20 +0300

    Fixed Some Issues

--- add_object_to_image.py
+++ add_object_to_image.py
@@ -4,7 +4,7 @@ import numpy as np
 image = np.zeros((512, 512, 3), np.uint8)
 image[:] = 255, 0, 0
 
-# Add Shapes To An Image
+# Add Shapes(Line, Rectangle & Circle) To An Image
 
 cv2.line(image, (0, 0), (image.shape[1], image.shape[0]), (0, 255, 0), 3)
 cv2.rectangle(image, (0, 0), (250, 350), (0, 0, 255), 2)
